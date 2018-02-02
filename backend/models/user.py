8731960f04adb69324dd74808b2abff4143c611a
Tyler Lesmann <redhatcat@gmail.com>
2018-02-02 11:13:43 -0700

    Include proper password hashing

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,9 +1,9 @@
 from mongoengine import Document, ListField, StringField
 from passlib.hash import pbkdf2_sha256
+from app import app
 
 def hash_password(password):
-    # TODO real salt from config
-    return pbkdf2_sha256.using(salt=b'super-secret').hash(password)
+    return pbkdf2_sha256.using(salt=bytes(app.config['SECRET_KEY'], 'utf-8')).hash(password)
 
 class User(Document):
     username = StringField(max_length=32, required=True)
